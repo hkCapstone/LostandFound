91ddc41936daf49f0ecd718d2092febc60a9d485
David <kimeryd256@gmail.com>
2016-05-25 08:51:23 -0700

    Final Version: Insert does not work, error in php code

---
+++ app/src/main/java/com/hk/lostandfound/Config.java
@@ -0,0 +1,10 @@
+package com.hk.lostandfound;
+
+public class Config {
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://ec2-54-191-44-221.us-west-2.compute.amazonaws.com/upload_image.php";
+
+    // Directory name to store captured images and videos
+    //public static final String IMAGE_DIRECTORY_NAME = "Lost and Found: Pet Finder";
+    public static final String IMAGE_DIRECTORY_NAME = "LaFPF";
+}
